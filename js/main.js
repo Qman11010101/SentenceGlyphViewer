49aba98aee0120cec58b324f00e7f427646f6f41
Kjuman Enobikto <30486982+Qman11010101@users.noreply.github.com>
2023-01-19 03:03:24 +0900

    first

---
+++ js/main.js
@@ -0,0 +1,29 @@
+const getData = async function (url) {
+    const res = await fetch(url);
+    return await res.json();
+};
+
+async function submit() {
+    const sentence = [...document.getElementById("inse-sentence").value];
+    const tableSection = document.getElementById("glyph-table");
+
+    // Make the table empty
+    const objs = tableSection.childElementCount;
+    for (let c = 0; c < objs; c++) {
+        tableSection.firstElementChild.remove();
+    }
+
+    sentence.forEach(c => {
+        let codeHex = c.codePointAt(0).toString(16).toUpperCase().padStart(4, "0");
+        let charName = data[codeHex];
+        let glyphLine = tableSection.appendChild(document.createElement("tr"));
+        let glyph = glyphLine.appendChild(document.createElement("td"));
+        glyph.innerText = c;
+        let codepoint = glyphLine.appendChild(document.createElement("td"));
+        codepoint.innerText = "U+" + codeHex;
+        let name = glyphLine.appendChild(document.createElement("td"));
+        name.innerText = charName;
+    });
+
+    tableSection.style.display = "table";
+}
