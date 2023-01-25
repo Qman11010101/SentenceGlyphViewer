f66a3013826594b89e28bdd90aa66b57918c83ec
キューマン・エノビクト <30486982+Qman11010101@users.noreply.github.com>
2023-01-25 21:32:29 +0900

    final

--- docs/js/main.js
+++ docs/js/main.js
@@ -32,3 +32,7 @@ async function analyze() {
 
     tableSection.style.display = "table";
 }
+
+window.addEventListener('DOMContentLoaded', () => {
+    document.getElementById("modify").innerText = modifyTime;
+});
