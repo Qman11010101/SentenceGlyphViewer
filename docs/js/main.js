7395866f918325441e4abff325ca128ab6d5f506
キューマン・エノビクト <30486982+Qman11010101@users.noreply.github.com>
2025-07-03 13:07:43 +0900

    コードの整形と冗長なループの削除

--- docs/js/main.js
+++ docs/js/main.js
@@ -1,38 +1,35 @@
 const getData = async function (url) {
-    const res = await fetch(url);
-    return await res.json();
-};
+    const res = await fetch(url)
+    return await res.json()
+}
 
 async function analyze() {
-    const sentence = [...document.getElementById("inse-sentence").value];
-    const tableSection = document.getElementById("glyph-table");
+    const sentence = [...document.getElementById("inse-sentence").value]
+    const tableSection = document.getElementById("glyph-table")
 
     // Make the table empty
-    const objs = tableSection.childElementCount;
-    for (let c = 0; c < objs; c++) {
-        tableSection.firstElementChild.remove();
-    }
+    while (tableSection.firstChild) tableSection.removeChild(tableSection.firstChild)
 
     if (sentence.length === 0) {
-        tableSection.style.display = "none";
-        return;
-    };
+        tableSection.style.display = "none"
+        return
+    }
 
     sentence.forEach(c => {
-        let codeHex = c.codePointAt(0).toString(16).toUpperCase().padStart(4, "0");
-        let charName = data[codeHex];
-        let glyphLine = tableSection.appendChild(document.createElement("tr"));
-        let glyph = glyphLine.appendChild(document.createElement("td"));
-        glyph.innerText = c;
-        let codepoint = glyphLine.appendChild(document.createElement("td"));
-        codepoint.innerText = "U+" + codeHex;
-        let name = glyphLine.appendChild(document.createElement("td"));
-        name.innerText = charName;
-    });
+        let codeHex = c.codePointAt(0).toString(16).toUpperCase().padStart(4, "0")
+        let charName = data[codeHex]
+        let glyphLine = tableSection.appendChild(document.createElement("tr"))
+        let glyph = glyphLine.appendChild(document.createElement("td"))
+        glyph.innerText = c
+        let codepoint = glyphLine.appendChild(document.createElement("td"))
+        codepoint.innerText = "U+" + codeHex
+        let name = glyphLine.appendChild(document.createElement("td"))
+        name.innerText = charName
+    })
 
-    tableSection.style.display = "table";
+    tableSection.style.display = "table"
 }
 
 window.addEventListener('DOMContentLoaded', () => {
-    document.getElementById("modify").innerText = modifyTime;
-});
+    document.getElementById("modify").innerText = modifyTime
+})
